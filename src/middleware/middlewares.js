95c2ed99e4fa8241dae3daaaaf04697c0eded8bf
Oxunjon <oxunjonxatamov32@gmail.com>
2024-01-30 12:25:44 +0500

    check user and admin

--- src/middleware/middlewares.js
+++ src/middleware/middlewares.js
@@ -1,40 +1,90 @@
+import { roles } from "../common/enums/roles.js";
+import { ResData } from "../common/resData.js";
 import { verifyToken } from "../lib/jwt.js";
-import { TokenRequiredException } from "./exception/middleware.exception.js";
-import { UserService } from "../modules/user/user.service.js";
+import { UserService } from "../modules/users/user.service.js";
+import {
+  AuthorizationTokenRequiredException,
+  AuthorizationUserIdRequiredException,
+  ForbidenAdminRoleException,
+  ForbidenUserRoleException,
+} from "./exception/middleware.exception.js";
 
-const userService = new UserService();
-
-export class Middleware {
+export class AuthorizationMiddleware {
   async checkToken(req, res, next) {
     try {
-      const token = req.cookies?.token;
+      const token = req.headers?.token;
 
       if (!token) {
-        throw new TokenRequiredException();
+        throw new AuthorizationTokenRequiredException();
       }
 
-      const userId = verifyToken(token);
+      const { data: userId } = verifyToken(token);
+
+      if (!userId) {
+        throw new AuthorizationUserIdRequiredException();
+      }
+
       req.userId = userId;
 
       return next();
     } catch (error) {
-      res.render("login", {
-        data: { isVisible: true, message: error.message },
-      });
+      const resData = new ResData(error.message, 401, null, error);
+
+      res.status(resData.statusCode).json(resData);
     }
   }
 
-  async currentUser(req, res, next) {
+  async checkUser(req, res, next) {
     try {
-      const userId = req.userId;
-      const currentUser = await userService.getUserById(userId);
+      const userId = Number(req.userId);
 
-      req.currentUser = currentUser.data;
+      if (!userId) {
+        throw new AuthorizationUserIdRequiredException();
+      }
+
+      const userService = new UserService();
+
+      const { data: currentUser } = await userService.getById(userId);
+
+      req.currentUser = currentUser;
+
       return next();
     } catch (error) {
-      res.render("register", {
-        data: { isVisible: true, message: error.message },
-      });
+      const resData = new ResData(error.message, 401, null, error);
+
+      res.status(resData.statusCode).json(resData);
+    }
+  }
+
+  async checkAdminUser(req, res, next) {
+    try {
+      const currentUser = req.currentUser;
+
+      if (currentUser.role !== roles.USER) {
+        throw new ForbidenUserRoleException();
+      }
+
+      return next();
+    } catch (error) {
+      const resData = new ResData(error.message, error.statusCode, null, error);
+
+      res.status(resData.statusCode).json(resData);
+    }
+  }
+
+  async checkAdminRole(req, res, next) {
+    try {
+      const currentUser = req.currentUser;
+
+      if (currentUser.role !== roles.ADMIN) {
+        throw new ForbidenAdminRoleException();
+      }
+
+      return next();
+    } catch (error) {
+      const resData = new ResData(error.message, error.statusCode, null, error);
+
+      res.status(resData.statusCode).json(resData);
     }
   }
 }
